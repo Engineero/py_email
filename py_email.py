d584851e9c594ca381644ec12ff965bad21e2bd3
Engineero <engineerolabs@gmail.com>
2020-08-31 21:17:58 -0600

    Added arg checking, moved some arg priorities around.

--- py_email.py
+++ py_email.py
@@ -10,6 +10,7 @@ Can also be used to send a text message using the appropriate carrier's
 domain."""
 
 
+import re
 from argparse import ArgumentParser
 from smtplib import SMTP
 from email.message import EmailMessage
@@ -18,7 +19,11 @@ from email.message import EmailMessage
 def check_args(args):
     """Checks arguments, throws errors if there are problems."""
 
-    pass
+    pattern = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    if not re.search(pattern, args.to_addr):
+        raise ValueError('Invalid To address!')
+    if not re.search(pattern, args.from_addr):
+        raise ValueError('Invalid From address!')
 
 
 def send_message(msg, server):
@@ -32,10 +37,11 @@ def main(args):
 
     """Compiles and sends the email message."""
     msg = EmailMessage()
-    msg.set_content(args.message)
     msg['Subject'] = args.subject
     msg['From'] = args.from_addr
     msg['To'] = args.to_addr
+    if args.message is not None:
+        msg.set_content(args.message)
     send_message(msg, args.server)
     print('Message sent!')
 
@@ -47,10 +53,10 @@ if __name__ == '__main__':
                         help='Destination email address.')
     parser.add_argument('from_addr', type=str,
                         help="Sender's email address.")
-    parser.add_argument('message', type=str,
-                        help='Email message content.')
-    parser.add_argument('--subject', type=str, default='Hi!',
-                        help='Email message subject. Default is "Hi!"')
+    parser.add_argument('subject', type=str,
+                        help='Email message subject.')
+    parser.add_argument('--message', type=str,
+                        help='Email message content. Default is None.')
     parser.add_argument('--server', type=str, default='localhost',
                         help='SMTP server address. Default is "localhost".')
 
